16b3133afc158edb580a47809302d5626c382ec4
System Administrator <root@Pavels-MacBook-Pro.local>
2021-06-21 03:35:41 +0300

    Addec Project Files. Fixed some typos.

--- ecom/settings.py
+++ ecom/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_FILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 MEDIA_URL = '/MEDIA/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
